c53f78eb8fd7b435aaa48e0d4173a14b663c6ca4
BenaSPACE <BenaSPACE@gmail.com>
2023-08-30 20:05:24 -0400

    job done

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,27 +1,47 @@
-const User = require('../models/User')
+const User = require('../models/User');
+
+//handle errors
+const handleErrors = err => {
+    console.log(err.message, err.code);
+
+    let errors = { email: '', password: '' };
+
+    //duplicate error code
+    if (err.code === 11000) {
+        errors.email = 'that email is already registered'
+        return errors;
+    }
+
+    //validation errors
+    if (err.message.includes('user validation failed')) {
+        Object.values(err.errors).forEach(({properties}) => {
+            errors[properties.path] = 'some value';
+        });
+    }
+};
 
 module.exports.signup_get = (req, res) => {
     res.render('signup');
-}
+};
 module.exports.login_get = (req, res) => {
     res.render('signup');
-}
+};
 module.exports.signup_post = async (req, res) => {
     const { email, password } = req.body;
-    
-    try{
-        const user = await User.create({email, password});
+
+    try {
+        const user = await User.create({ email, password });
         res.status(201).json(user);
     }
-    catch(err){
-        console.log(err);
-        res.status(400).send('error, user not created');
+    catch (err) {
+        handleErrors(err);
+        res.status(400).json({ errors });
     }
 
-}
+};
 module.exports.login_post = async (req, res) => {
     const { email, password } = req.body;
     console.log(email, password);
 
     res.render('signup');
-}
+};
--- models/User.js
+++ models/User.js
@@ -1,17 +1,31 @@
 const mongoose = require('mongoose');
+const { isEmail } = require('validator');
 
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
-        required: true,
+        required: [true, 'Please enter an email'],
         unique: true,
-        lowercase: true
+        lowercase: true,
+        validate: [isEmail, 'Please enter a valid email']
     },
     password: {
         type: String,
-        required: true,
-        minLength: 6
+        required: [true, 'please enter a password'],
+        minlength: [6, 'minimum password length is 6 characters']
     },
+});
+
+// fire a funciton after doc saved to db
+userSchema.post('save', function (doc, next) {
+    console.log('new user was created and saved');
+    next();
+});
+
+// fire a function before doc saved to db
+userSchema.pre('save', function (next) {
+    console.log('user soon to be created and saved', this);
+    next();
 })
 
 const User = mongoose.model('user', userSchema);
